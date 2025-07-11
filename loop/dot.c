29a9ca469ab4f8a2383243da749ae5678ec860fc
Gobinda Ghimire <gowingg1@gmail.com>
2025-07-11 04:58:02 -0800

    Update dot.c

--- loop/dot.c
+++ loop/dot.c
@@ -6,7 +6,8 @@ void main(){
     for(a=1;a<=20;a++){
     sum=sum+((2+x)/a);
     if(a<=19){
-    printf("(2+x)/%g,  ",a);
+    printf("(2+x)/%g,  ",a); // %g prints float neatly: e.g., 5.000000 becomes 5, 0.00000123 becomes 1.23e-06
+
     }else{
     printf("(2+x)/%g.",a);
     }
